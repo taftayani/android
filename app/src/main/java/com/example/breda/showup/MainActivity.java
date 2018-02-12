ea49f97700435814db4d36cc74fd1b9ea7131997
anawahidah <anawahidahmanurung@gmail.com>
2018-02-12 15:10:41 +0700

    ubah text view

--- app/src/main/java/com/example/breda/showup/MainActivity.java
+++ app/src/main/java/com/example/breda/showup/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.breda.showup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        word = (TextView) findViewById(R.id.textView);
+
+        word.setText("aku ana looo");
     }
 }
