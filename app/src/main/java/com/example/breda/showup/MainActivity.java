915a0c16d92433fe4144de4f2037b0eb5cf1eeb2
rafrialdi <35679782+rafrialdi@users.noreply.github.com>
2018-03-20 14:15:17 +0700

    perbaikan

--- app/src/main/java/com/example/breda/showup/MainActivity.java
+++ app/src/main/java/com/example/breda/showup/MainActivity.java
@@ -1,20 +1,7 @@
 package com.example.breda.showup;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
-
-    public void onSignUp(View view) {
-        Intent intent = new Intent(this, SignUp.class);
-        startActivity(intent);
-    }
+   
 }
